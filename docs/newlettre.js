d9b39fab81f8536eb8cacba9c4c8635e979533ff
edeltechs <72034717+edeltechs@users.noreply.github.com>
2020-10-29 12:17:25 +0100

    Add files via upload

---
+++ docs/newlettre.js
@@ -0,0 +1,14 @@
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+var config = {
+    apiKey: "AIzaSyDCdO_PElJabj8bV2HhEjNZkJ9KeLQShPE",
+    authDomain: "edeltech-b5870.firebaseapp.com",
+    databaseURL: "https://edeltech-b5870.firebaseio.com",
+    storageBucket: "edeltech-b5870.appspot.com"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+
